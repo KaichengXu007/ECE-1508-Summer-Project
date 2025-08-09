06e8d79f42288f31845c5194e5b222bfb3a4fb16
Kassy <kassyzxl@gmail.com>
2025-08-09 00:06:34 -0400

    cleaned the code

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ from src.train import train
 
 def main():
     # 超参数设置
-    epochs = 300          # 训练的总epoch数
-    batch_size = 16       # 批次大小
+    epochs = 200         # 训练的总epoch数
+    batch_size = 64       # 批次大小
     latent_dim = 150      # 噪声向量维度
-    lr_D = 0.0001           # 学习率
-    lr_G = 0.0004
+    lr_D = 0.0002           # 学习率
+    lr_G = 0.0002
+    resume_epoch = None
 
 
     # 设备选择
@@ -25,14 +26,13 @@ def main():
     # parquet = './data/roberta-base_train_caps.parquet'
     parquet = './data/fashion_CLIP_train_caps.parquet'
     img_dir = './data'
-    # embed_dim = 768
     embed_dim = 512
 
     # 运行训练
     train(
         parquet, img_dir,
         epochs, batch_size, lr_D, lr_G, latent_dim,
-        embed_dim
+        embed_dim, resume_epoch
     )
 
 if __name__ == "__main__":
